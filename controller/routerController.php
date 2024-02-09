311768a99d12ed1dd01d3fb0a4cc823b9e54d1f6
mikhawa <michaeljpitz@gmail.com>
2024-02-09 16:38:03 +0100

    comment router

--- controller/routerController.php
+++ controller/routerController.php
@@ -4,6 +4,7 @@ Notre router
 */
 // s'il existe la variable get section
 if (isset($_GET['section'])) {
+    // on compare la valeur de section et on crée la route
     switch ($_GET['section']) {
         case "travaux":
             $route = "travaux.html.php";
@@ -12,6 +13,7 @@ if (isset($_GET['section'])) {
             $route = "contact.html.php";
             break;
         case "livredor":
+            // on charge le contrôleur de gestion du livre d'or
             require "livredorController.php";
             $route = "livredor.html.php";
             break;
